d5f3167b87d210235fdb566642973a6dd7c66c53
milex <milex@LAPTOP-JDG0N7NP>
2021-12-23 09:25:17 -0500

    Agregar archivos de proyecto.

---
+++ acceso a datos/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace acceso_a_datos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtcedula = new System.Windows.Forms.TextBox();
+            this.txtapellidos = new System.Windows.Forms.TextBox();
+            this.txtnombres = new System.Windows.Forms.TextBox();
+            this.txtfechadenacimiento = new System.Windows.Forms.TextBox();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingreso de datos de personas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa la cedula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingresa los apellidos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese los nombres";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ingrese la fecha de nacimeinto";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ingrese el peso en  kilogramos";
+            // 
+            // txtcedula
+            // 
+            this.txtcedula.Location = new System.Drawing.Point(209, 88);
+            this.txtcedula.Name = "txtcedula";
+            this.txtcedula.Size = new System.Drawing.Size(252, 22);
+            this.txtcedula.TabIndex = 6;
+            // 
+            // txtapellidos
+            // 
+            this.txtapellidos.Location = new System.Drawing.Point(209, 146);
+            this.txtapellidos.Name = "txtapellidos";
+            this.txtapellidos.Size = new System.Drawing.Size(253, 22);
+            this.txtapellidos.TabIndex = 7;
+            // 
+            // txtnombres
+            // 
+            this.txtnombres.Location = new System.Drawing.Point(209, 200);
+            this.txtnombres.Name = "txtnombres";
+            this.txtnombres.Size = new System.Drawing.Size(253, 22);
+            this.txtnombres.TabIndex = 8;
+            // 
+            // txtfechadenacimiento
+            // 
+            this.txtfechadenacimiento.Location = new System.Drawing.Point(209, 242);
+            this.txtfechadenacimiento.Name = "txtfechadenacimiento";
+            this.txtfechadenacimiento.Size = new System.Drawing.Size(252, 22);
+            this.txtfechadenacimiento.TabIndex = 9;
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Location = new System.Drawing.Point(209, 289);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(252, 22);
+            this.txtpeso.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 78);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 72);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "nuevo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpeso);
+            this.Controls.Add(this.txtfechadenacimiento);
+            this.Controls.Add(this.txtnombres);
+            this.Controls.Add(this.txtapellidos);
+            this.Controls.Add(this.txtcedula);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtcedula;
+        private System.Windows.Forms.TextBox txtapellidos;
+        private System.Windows.Forms.TextBox txtnombres;
+        private System.Windows.Forms.TextBox txtfechadenacimiento;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
